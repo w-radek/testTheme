b4b418c541f5f6bcadc423f2ad063f8dd6983311
w-radek <radoslawysocki@gmail.com>
2021-07-29 15:28:23 +0200

    Iit

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title><?php wp_title( ' | ', TRUE, 'right' ); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+
+        <?php $ver = filemtime( get_template_directory() . '/public/css/app.min.css' ); ?>
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ); ?>/public/css/app.min.css?v=<?php echo $ver; ?>" media="screen"/>
+
+        <link rel="shortcut icon" href="<?php bloginfo( 'template_url' ); ?>/public/images/favicon.ico" type="image/x-icon">
+        <link rel="icon" href="<?php bloginfo( 'template_url' ); ?>/public/images/favicon.ico" type="image/x-icon">
+        
+        <?php wp_head(); ?>
+        
+    </head>
+
+    <body <?php body_class(); ?>>
+        <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please
+            <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
---
+++ tpl-blank.php
@@ -0,0 +1,6 @@
+<?php /* Template Name: Template name */ ?>
+<?php get_header(); ?>
+<?php 
+    get_template_part('sections/');
+?>
+<?php get_footer(); ?>
